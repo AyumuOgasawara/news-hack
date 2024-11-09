ece6018f6ea2b8a1574d0082efa7f623858f6350
Anna Harding <anna.harding.1@effem.com>
2024-11-09 16:12:30 +0900

    adds dropdown

--- frontend/news-hack/src/App.jsx
+++ frontend/news-hack/src/App.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default () => (
+export default () => {
+  const [frequency, setFrequency] = React.useState("Choose frequency")
+
+  const handleEveryday = () => {
+    setFrequency("Everyday");
+    console.log(frequency);
+  };
+  const handleEveryWeekday = () => {
+    setFrequency("Every weekday");
+    console.log(frequency);
+  };
+  const handleEveryWeekend = () => {
+    setFrequency("Every weekend");
+    console.log(frequency);
+  };
+  return (
   <>
     <h1 style={{fontWeight:"bold", fontFamily: "monospace"
     }}>news hack</h1>
+
+    <DropdownButton id="dropdown-basic-button" title={frequency} style={{fontWeight:"bold", fontFamily: "monospace"
+    }}>
+      <Dropdown.Item onClick={handleEveryday}>Everyday</Dropdown.Item>
+      <Dropdown.Item onClick={handleEveryWeekday}>Every weekday</Dropdown.Item>
+      <Dropdown.Item onClick={handleEveryWeekend}>Every weekend</Dropdown.Item>
+    </DropdownButton>
+
   </>
 );
+}

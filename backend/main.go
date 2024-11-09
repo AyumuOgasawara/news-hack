369e22beb8294e65964bf570b3a5dc421b16385f
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-11-09 22:00:15 +0900

    ✨Made POST /articles endpoint

--- backend/main.go
+++ backend/main.go
@@ -30,6 +30,13 @@ type NewsApiResponse struct {
 	Articles     []Article `json:"articles"`
 }
 
+// POST Request Structure
+type ArticleRequest struct {
+	Keywords    string `json:"keyword" binding:"required"`
+	NumArticles int    `json:"numArticles" binding:"required"`
+	PhoneNumber string `json:"phoneNumber" binding:"required"`
+}
+
 func main() {
 	// load .env
 	err := godotenv.Load()
@@ -85,6 +92,38 @@ func main() {
 
 		// Return news response
 		c.JSON(http.StatusOK, newsResponse)
+	})
+
+	// POST /articles endpoint
+	router.POST("/articles", func(c *gin.Context) {
+		// Bind to ArticleRequest Structure
+		var req ArticleRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
+			return
+		}
+
+		// make News API url
+		newsURL := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&apiKey=%s",
+			req.Keywords, req.NumArticles, newsApiKey)
+
+		resp, err := http.Get(newsURL)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch news"})
+			return
+		}
+		defer resp.Body.Close()
+
+		// Decode Response
+		var newsResponse NewsApiResponse
+		if err := json.NewDecoder(resp.Body).Decode(&newsResponse); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode news data"})
+			return
+		}
+
+		for i := 0; i < req.NumArticles; i++ {
+			fmt.Printf("This is article title %s", newsResponse.Articles[i].Title)
+		}
 	})
 
 	// run server
